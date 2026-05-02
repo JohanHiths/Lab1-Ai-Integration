d7e31cf4d80989789ba5669cb0cf46420fe09e85
JohanHiths <johan.hietala@iths.se>
2026-05-02 13:52:37 +0200

    import

--- src/main/kotlin/ai/ai/service/OpenRouterTestService.kt
+++ src/main/kotlin/ai/ai/service/OpenRouterTestService.kt
@@ -2,6 +2,7 @@ package ai.ai.service
 
 
 import ai.ai.dto.OpenRouterResponse
+import ai.ai.exception.InsufficientCreditsException
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
